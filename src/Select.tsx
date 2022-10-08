b2e7ff8fb11ead3ce0a58a98fe844692960c249c
Iman Anooshehpour <imananoosheh@gmail.com>
2022-10-08 15:54:59 -0700

    Implemented Keyboard Event intractions

--- src/Select.tsx
+++ src/Select.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./select.module.css";
 
 export type SelectOption = {
@@ -25,6 +25,7 @@ type SelectProps = {
 export function Select({ multiple, value, onChange, options }: SelectProps) {
   const [isOpen, setIsOpen] = useState(false);
   const [highlightedIndex, setHighlightedIndex] = useState(0);
+  const containerRef = useRef<HTMLDivElement>(null);
 
   function clearOptions() {
     multiple ? onChange([]) : onChange(undefined);
@@ -49,8 +50,43 @@ export function Select({ multiple, value, onChange, options }: SelectProps) {
     if (isOpen) setHighlightedIndex(0);
   }, [isOpen]);
 
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.target != containerRef.current) return;
+      switch (e.code) {
+        case "Enter":
+        case "Space":
+          setIsOpen((prev) => !prev);
+          if (isOpen) selecOption(options[highlightedIndex]);
+          break;
+        case "ArrowUp":
+        case "ArrowDown": {
+          if (!isOpen) {
+            setIsOpen(true);
+            break;
+          }
+
+          const newValue = highlightedIndex + (e.code === "ArrowDown" ? 1 : -1);
+          if (newValue >= 0 && newValue < options.length) {
+            setHighlightedIndex(newValue);
+          }
+          break;
+        }
+        case "Escape":
+          setIsOpen(false);
+          break;
+      }
+    };
+    containerRef.current?.addEventListener("keydown", handler);
+
+    return () => {
+      containerRef.current?.removeEventListener("keydown", handler);
+    };
+  }, [isOpen, highlightedIndex, options]);
+
   return (
     <div
+      ref={containerRef}
       onBlur={() => setIsOpen(false)}
       onClick={() => setIsOpen((pre) => !pre)}
       tabIndex={0}
